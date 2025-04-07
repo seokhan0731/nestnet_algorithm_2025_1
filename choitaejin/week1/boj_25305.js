ffa2577b816571344dc2d48d10aea9b443e9d4e2
taejinchoi-cbnu <xowls000@chungbuk.ac.kr>
2025-04-07 13:29:27 +0900

    refactor

--- choitaejin/week1/boj_25305.js
+++ choitaejin/week1/boj_25305.js
@@ -24,14 +24,9 @@ rl.on('line', (line) => {
 
 
 }).on('close', () => {
-    score.sort((a,b) => b-a);
+  score.sort((a,b) => b-a);
 
-    if (score.length >= K) {
-        console.log(score[K - 1]);
-    }
-    else {
-        console.log("점수가 부족합니다.");
-    }
+  console.log(score[K - 1]);
 
   process.exit();
 });
